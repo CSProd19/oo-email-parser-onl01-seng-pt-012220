69e6ce35fa87da6479a3bfbf61a5478fd3d50c1e
Maria Connie Sanchez <msnchz04@gmail.com>
2020-02-20 22:42:37 +0000

    Automatically backed up by Learn

--- lib/email_parser.rb
+++ lib/email_parser.rb
@@ -4,7 +4,12 @@
 # or whitespace (' ').
 
 class EmailAddressParser
-  attr_accessor :email
+  attr_accessor :email_addresses
+  
+  def initialize(email_addresses)
+    
+  end
+  
   def parse 
     
   end 
